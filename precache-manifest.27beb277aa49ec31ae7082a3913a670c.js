c3c73606d1b10f4d6b41aba2a4dd8eadc5d595bb
Partha Roy <parth@Parthas-MacBook-Pro.local>
2020-01-10 11:26:28 +0530

    Updates

---
+++ precache-manifest.27beb277aa49ec31ae7082a3913a670c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb0aeba306344321907c0b4d800956a",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "d953ce4de4ba1da73af6",
+    "url": "/To-Do-App/static/css/main.6eeca507.chunk.css"
+  },
+  {
+    "revision": "b98210248e14e4537f72",
+    "url": "/To-Do-App/static/js/2.2ea8ef4c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/To-Do-App/static/js/2.2ea8ef4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d953ce4de4ba1da73af6",
+    "url": "/To-Do-App/static/js/main.f1c6ae83.chunk.js"
+  },
+  {
+    "revision": "95e50ad5aca939ec0c9c",
+    "url": "/To-Do-App/static/js/runtime-main.9512e5d0.js"
+  }
+]);
